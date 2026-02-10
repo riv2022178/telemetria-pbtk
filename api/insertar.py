9a98d39e84fdba0befb6437344ccb53f3625d04a
Ministerios Hechos <118035267+riv2022178@users.noreply.github.com>
2026-02-10 15:18:53 -0600

    Update insertar.py

--- api/insertar.py
+++ api/insertar.py
@@ -16,14 +16,16 @@ class handler(BaseHTTPRequestHandler):
             query = parse_qs(urlparse(self.path).query)
             temp = query.get('temperatura', [0])[0]
             id_disp = query.get('id', ['Termocupla_1'])[0]
+            # NUEVO: Capturar el valor de humedad enviado como 'valor_1'
+            humedad = query.get('valor_1', [None])[0] 
 
             # Conexión usando pymssql
             conn = pymssql.connect(**config)
             cursor = conn.cursor()
             
-            # Inserción en tu tabla real
-            query_sql = "INSERT INTO dbo.measurements_receipt (fecha_hora, dispositivo_id, valor) VALUES (GETDATE(), %s, %s)"
-            cursor.execute(query_sql, (id_disp, temp))
+            # MODIFICADO: Ahora insertamos también en la columna valor_1
+            query_sql = "INSERT INTO dbo.measurements_receipt (fecha_hora, dispositivo_id, valor, valor_1) VALUES (GETDATE(), %s, %s, %s)"
+            cursor.execute(query_sql, (id_disp, temp, humedad))
             
             conn.commit()
             conn.close()
@@ -31,7 +33,7 @@ class handler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write("EXITO_SQL".encode())
+            self.wfile.write("EXITO_SQL_CON_HUMEDAD".encode())
             
         except Exception as e:
             self.send_response(500)
